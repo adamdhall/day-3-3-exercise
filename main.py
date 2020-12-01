22c5ee1775f37c66be2cd4a283085a332655694f
adamdhall <adamdhall@outlook.com>
2020-12-01 19:44:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year % 4 == 0 and year % 100 > 0:
+  print("Leap year.")
+elif year %400 == 0:
+  print("Leap year.")
+else:
+  print ("Not leap year.")
